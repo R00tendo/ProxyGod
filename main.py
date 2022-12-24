4977f6c33371a08e6d7777e28a0e1e5e525f5f83
R00tendo <72181445+R00tendo@users.noreply.github.com>
2022-12-24 12:40:00 -0500

    0.2
    cycles added

--- main.py
+++ main.py
@@ -6,34 +6,53 @@ import argparse
 import os
 import sys
 
+def info(msg):
+    if not args.quiet:
+        print(c(msg, "blue"))
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--proxies', help='File containing all the proxies in this format: proto://ip:port', required=True)
     parser.add_argument('-t', '--threads', help='Amount of threads.', required=False, default=40)
     parser.add_argument('-f', '--format', help='Format of output. Options: 0: proto://ip:port 1:proto ip port', required=True, type=int)
     parser.add_argument('-q', '--quiet', help='Only displays the final results.', required=False, default=False, action="store_true")
+    parser.add_argument('-c', '--cycles', help='How many times proxygod will check the proxies (helps to discovery stable proxies)', type=int, default=1)
 
     args = parser.parse_args()
     return args
 
 def thread(proxy):
-    try:
-        r = requests.get("https://www.whatismyip.net/", proxies=dict(http=proxy, https=proxy), timeout=2)
-        if r.status_code == 200 and "What Is My IP | Whats My IP Address | GeoIP Location | Check IP Information | IP Tools" in r.text:
-             variables.working.append(proxy)
-             if not args.quiet:
-                 print(c(f"📡 {proxy}{' ' * (40 - len(proxy) + len(str(variables.overall) + '/' + str(variables.overall)))}{''}", 'green'))
-    except KeyboardInterrupt:
-        sys.exit()
-    except:
-        if not args.quiet:
-            print(c(f"🌹🪦🌹 {proxy} {' ' * (40 - (len(proxy) + 4) )} {variables.current}/{variables.overall}{' ' * 20}", 'red'), end='\r')
-        pass
+    if len(proxy) > 2:
+        try:
+            formatt = c(f"🌹🪦🌹 {proxy} {' ' * (40 - (len(proxy) + 4) )} {variables.current}/{variables.overall}{' ' * 20}", 'red')
 
+            r = requests.get("https://www.whatismyip.net/", proxies=dict(http=proxy, https=proxy), timeout=2)
+            if r.status_code == 200 and "What Is My IP | Whats My IP Address | GeoIP Location | Check IP Information | IP Tools" in r.text:
+                 variables.working.append(proxy)
+                 if not args.quiet:
+                     print(c(f"📡 {proxy} {' ' * 50}", 'green'))
+            r = ""
+        except KeyboardInterrupt:
+            sys.exit()
+
+        except requests.exceptions.ConnectionError:
+            if not args.quiet:
+                print(formatt)
+            pass
+
+        except requests.exceptions.ReadTimeout:
+            if not args.quiet:
+                print(formatt)
+            pass
+
+        except Exception as e:
+            print(e)
+            
     variables.cur_running -= 1
+    exit()
 
 def main(args):
-
+    info("🔄Cycle 1🔄")
     variables.overall = len(open(args.proxies).read().split("\n"))
     for proxy in open(args.proxies).read().split("\n"):
         variables.current += 1
@@ -48,22 +67,62 @@ def main(args):
         variables.cur_running += 1    
 
     while variables.cur_running != 0:
-      try:
-          time.sleep(0.2)
-      except KeyboardInterrupt:
-          sys.exit()
-      
-    os.system("clear")    
+        try:
+            time.sleep(0.2)
+        except KeyboardInterrupt:
+            sys.exit()
+
+
+#    os.system("clear")    
+
+
+
+
+    for cycle in range(2,args.cycles):
+
+        working_temp = variables.working
+        variables.working = []
+
+        info(f"🔄Cycle {cycle}🔄")
+
+        variables.current = 0
+        variables.overall = len(working_temp)
+
+        for proxy in working_temp:
+            variables.current += 1
+            while variables.cur_running >= variables.threads:
+                try:
+                    time.sleep(0.2)
+                except KeyboardInterrupt:
+                    sys.exit()    
+
+            threading.Thread(target=thread, args=(proxy,)).start()
+            
+            variables.cur_running += 1   
+        while variables.cur_running != 0:
+            try:
+                time.sleep(0.2)
+            except KeyboardInterrupt:
+                sys.exit()
+
+
+#        os.system("clear")
 
     if args.format == 1:
         for proxy in variables.working:
-            print(proxy.replace('://', ' ').replace(':', ' '))    
+            formatted = proxy.replace('://', ' ').replace(':', ' ')
+            if len(formatted) > 1:
+                print(formatted.strip())
 
-    if args.format == 0:
+    elif args.format == 0:
         for proxy in variables.working:
-            print(proxy)
-
-
+            if len(proxy) > 1:
+                print(proxy.strip())
+    
+    else:
+        print("Invalid format...")
+        sys.exit(1)
+    
 if __name__ == "__main__":
    args = get_args()
 
@@ -73,5 +132,5 @@ if __name__ == "__main__":
     overall = 0
     current = 0
     threads = int(args.threads)
-
+   
    main(args)
